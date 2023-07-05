3d0ba4520137d83495acb151ebb2fcab81fd5cd1
Huayi Tian <89336601+HappyTTian@users.noreply.github.com>
2023-07-05 02:16:23 -0700

    game logic done

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let scoreDisplay = document.querySelector(".scoreDisplay");
 let left = document.querySelector(".left");
 let bottom = document.querySelector(".bottom");
 let right = document.querySelector(".right");
-let up = document.querySelector(".top");
+let up = document.querySelector(".top"); //button for mobile devices
 let width = 10;
 let currentIndex = 0;
 let appleIndex = 0;
@@ -14,7 +14,7 @@ let currentSnake = [2, 1, 0];
 let direction = 1;
 let score = 0;
 let speed = 0.8;
-let intervalTime = 1000;
+let intervalTime = 500;
 let interval = 0
 
 //directions of right
@@ -22,6 +22,19 @@ const s_right = 1;
 const s_left = -1;
 const s_up = -width;
 const s_down = width;
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.addEventListener("keyup", control);
+    up.addEventListener("click", () => (direction = s_up));
+    bottom.addEventListener("click", () => (direction = s_down));
+    left.addEventListener("click", () => (direction = s_left));
+    right.addEventListener("click", () => (direction = s_right));
+    createBoard();
+    startGame();
+    playAgain.addEventListener("click", replay);
+});
 
 function createBoard(){
    popup.style.display = "none"; // hide the replay button first
@@ -34,22 +47,13 @@ function createBoard(){
 }
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    document.addEventListener("keyup", control);
-    createBoard();
-    startGame();
-    playAgain.addEventListener("click", replay);
-});
-
-
-
 function startGame(){
     let squares = document.querySelectorAll(".grid div");
-    randomApple(squares);// random position of apple
-    direction = 1; //set the initial direction of snake
+    direction = s_right; //set the initial direction of snake
     scoreDisplay.innerHTML = score;
     currentSnake = [2, 1, 0];
     currentIndex = 0;
+    randomApple(squares);// random position of apple
     currentSnake.forEach((index) => squares[index].classList.add("snake"));
     interval = setInterval(moveResult, intervalTime);
 }
@@ -65,7 +69,16 @@ function moveResult(){
     }
 }
 function checkSnakeHits(squares){
-
+    
+    if((currentSnake[0] % width == width-1 && direction == s_right) //the snake hits the right border
+    || (currentSnake[0] % width == 0 && direction == s_left) //the snake hits the left border
+    || (currentSnake[0] < width && direction == s_up) //the snake hits the up border
+    || (currentSnake[0] >= (width*(width-1))  && direction == s_down)//the snake hits the down border
+    || (squares[currentSnake[0]+direction].classList.contains("snake")) //the snake hits it self
+    ){
+        return true;
+    }
+    return false;
 }
 function moveSnake(squares){
     let tail = currentSnake.pop();
@@ -75,11 +88,44 @@ function moveSnake(squares){
     eatApple(squares,tail); //check if snake eats the apple
     squares[new_head].classList.add("snake");
 }
-function control(){
+function eatApple(squares,tail){
+    if(squares[currentSnake[0]].classList.contains("apple")){
+        currentSnake.push(tail);
+        squares[currentSnake[0]].classList.remove("apple");
+        squares[tail].classList.add("snake");
+        score++; // increase the score
+        scoreDisplay.textContent = score;
+        randomApple(squares);
+        clearInterval(interval);
+        intervalTime *= speed; //increase speed of snake 
+        interval = setInterval(moveResult,intervalTime);
+    }
+}
 
+function randomApple(squares){
+    do{
+         appleIndex = Math.floor(Math.random()*squares.length);
+    }while(squares[appleIndex].classList.contains("snake")) //checks until it's not in the positions of snake
+
+    squares[appleIndex].classList.add("apple");
+}
+function control(input){
+    
+    if(input.keyCode === 39){
+        direction = s_right;
+    }else if(input.keyCode == 38){
+        direction = s_up;
+    }else if(input.keyCode == 37){
+        direction = s_left;
+    }else if(input.keyCode == 40){
+        direction = s_down;
+    }
 }
 
 function replay(){
-
+    grid.innerHTML = "";
+    createBoard();
+    startGame();
+    popup.style.display = "none";
 }
 
